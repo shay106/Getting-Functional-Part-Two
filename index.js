9766001d5f60073b067b9940c91145b724eb5676
Jonathan Corley <jcorley@noscihigh.org>
2021-03-30 15:46:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+//DO NOT CHANGE CODE HERE
+var customers = require('./customers.json');
+//DO NOT CHANGE CODE HERE
+console.log(customers)
+
+
+
+/*
+1. Oldest Customer
+
+Write a function that will find the oldest customer 
+and return their name.
+
+
+**HINT: Look at the data in the customer.json file. 
+
+* What kind of loop should you use to iterate over 
+the customers array?
+
+* What data type occupies each element of the 
+customers array? What type of loop should be used 
+then?
+
+
+*/
+
+
+function oldestCustomer(array){
+
+
+};
+
+
+
+/*
+2. Youngest Customer
+
+Write a function that will find the youngest customer and return their name.
+
+
+**HINT: Look at the data in the customer.json file. 
+
+* What kind of loop should you use to iterate over 
+the customers array?
+
+* What data type occupies each element of the 
+customers array? What type of loop should be used 
+then?
+
+*/
+
+function youngestCustomer(array){
+
+};
+
